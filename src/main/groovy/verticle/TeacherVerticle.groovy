747b7f012bf22731a20d1ff4c621eb1626660c3f
Nguyễn Quốc Kỳ <73680940+FlagNguyen@users.noreply.github.com>
2022-06-28 11:46:27 +0700

    Add docs

--- src/main/groovy/verticle/TeacherVerticle.groovy
+++ src/main/groovy/verticle/TeacherVerticle.groovy
@@ -1,7 +1,7 @@
 package verticle
 
 import entity.Teacher
-import groovy.json.JsonOutput
+import groovy.util.logging.Slf4j
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Promise
 import io.vertx.core.http.HttpServerResponse
@@ -13,20 +13,26 @@ import org.apache.commons.validator.routines.EmailValidator
 
 import java.util.stream.Collectors
 
+@Slf4j
 class TeacherVerticle extends AbstractVerticle {
 
-    private Map<String, Teacher> teachers = new HashMap<>()
+    /**
+     * Map contain:
+     * Key: Teacher ID <String>
+     * Value: Teacher object
+     */
+    private Map<String, Teacher> teachersByID = new HashMap<>()
 
     void createSampleData() {
-        teachers.put("1", new Teacher("1", "Mr. Obama", "obama@gmail.com"))
-        teachers.put("2", new Teacher("2", "Mr. Donald Trump", "trump@gmail.com"))
-        teachers.put("3", new Teacher("3", "Mr. Joe Biden", "biden@gmail.com"))
+        teachersByID.put("1", new Teacher("1", "Mr. Obama", "obama@gmail.com"))
+        teachersByID.put("2", new Teacher("2", "Mr. Donald Trump", "trump@gmail.com"))
+        teachersByID.put("3", new Teacher("3", "Mr. Joe Biden", "biden@gmail.com"))
+        log.info("Create sample data successfully")
     }
 
     @Override
     void start(Promise<Void> startPromise) throws Exception {
         createSampleData()
-        print teachers
         Router router = Router.router(vertx)
         router.get("/api/teachers").handler(this.&getAllTeachers)
         router.get("/api/teachers/:id").handler(this.&getTeacherByID)
@@ -47,12 +53,21 @@ class TeacherVerticle extends AbstractVerticle {
                         })
     }
 
+    /**
+     * Return list Json of teachers
+     * @param routingContext
+     */
     private void getAllTeachers(RoutingContext routingContext) {
         HttpServerResponse response = routingContext.response()
         response.putHeader("content-type", "application/json;charset=UTF-8")
-        response.end(Json.encodePrettily(teachers.values()))
+        response.end(Json.encodePrettily(teachersByID.values()))
     }
 
+    /**
+     * return teacher by input teacher id parameter
+     * @example-GET: http://localhost:8080/api/teachers/3
+     * @param routingContext
+     */
     private void getTeacherByID(RoutingContext routingContext) {
         HttpServerResponse response = routingContext.response()
         response.putHeader("content-type", "application/json;charset=UTF-8")
@@ -62,7 +77,7 @@ class TeacherVerticle extends AbstractVerticle {
             routingContext.response().setStatusCode(400).end()
         } else {
             try {
-                Teacher foundTeacher = teachers.get(teacherIdParam)
+                Teacher foundTeacher = teachersByID.get(teacherIdParam)
                 if (foundTeacher == null) {
                     routingContext.response().setStatusCode(400).end("Can't found this teacher")
                     return
@@ -75,6 +90,16 @@ class TeacherVerticle extends AbstractVerticle {
         }
     }
 
+    /**
+     * Method add new teacher by input Json
+     * @emxample-POST: http://localhost:8080/api/teachers
+     * @example-Json: {
+     "teacherID": "abc",
+     "teacherName": "Mr. Test",
+     "email": "test@gmail.com"
+     }
+     * @param routingContext
+     */
     private void addNewTeacher(RoutingContext routingContext) {
         HttpServerResponse response = routingContext.response()
         response.putHeader("content-type", "application/json;charset=UTF-8")
@@ -91,7 +116,7 @@ class TeacherVerticle extends AbstractVerticle {
                 response.end("Wrong format email")
                 return
             }
-            teachers.put(newTeacher.teacherID, newTeacher)
+            teachersByID.put(newTeacher.teacherID, newTeacher)
             println "Add new successfully $newTeacher"
             routingContext.response().setStatusCode(200).end(Json.encodePrettily(newTeacher))
         } catch (e) {
@@ -99,11 +124,17 @@ class TeacherVerticle extends AbstractVerticle {
         }
     }
 
+    /**
+     * Method validate input new teacher object
+     * @validation: ID not duplicate & follow email format
+     * @param newTeacher
+     * @return List error key messages
+     */
     private List validateTeacherRequestAndReturnMessage(Teacher newTeacher) {
         List outputErrorMessages = new ArrayList()
 
         //Get teacher ID list for checking duplicate
-        List<String> teacherIDList = teachers.values()
+        List<String> teacherIDList = teachersByID.values()
                 .stream()
                 .map({ teacher -> teacher.teacherID })
                 .collect(Collectors.toList())
@@ -118,24 +149,28 @@ class TeacherVerticle extends AbstractVerticle {
         return outputErrorMessages
     }
 
-
+    /**
+     * Method update teacher by id
+     * @input Json with id which teacher will be updated
+     * @param routingContext
+     */
     private void updateTeacherByID(RoutingContext routingContext) {
         HttpServerResponse response = routingContext.response()
         response.putHeader("content-type", "application/json;charset=UTF-8")
         try {
             Teacher updateTeacher = Json.decodeValue(routingContext.getBody(), Teacher.class)
 
-            Teacher foundTeacher = teachers.get(updateTeacher.teacherID)
+            Teacher foundTeacher = teachersByID.get(updateTeacher.teacherID)
             if (foundTeacher == null) {
                 response.end("This id doesn't existed")
                 return
             }
-            if(!EmailValidator.getInstance().isValid(updateTeacher.email)){
+            if (!EmailValidator.getInstance().isValid(updateTeacher.email)) {
                 response.end("Wrong email format ")
                 return
             }
 
-            teachers.replace(updateTeacher.teacherID, updateTeacher)
+            teachersByID.replace(updateTeacher.teacherID, updateTeacher)
             println "Update successfully $updateTeacher"
             routingContext.response().setStatusCode(200).end(Json.encodePrettily(updateTeacher))
         } catch (e) {
@@ -144,21 +179,28 @@ class TeacherVerticle extends AbstractVerticle {
         }
     }
 
+    /**
+     * Method delete teacher by id parameter
+     * @example-url: http://localhost:8080/api/teachers/abc (with "/abc" is id param)
+     * @param routingContext
+     */
     private void deleteTeacherByID(RoutingContext routingContext) {
         HttpServerResponse response = routingContext.response()
         response.putHeader("content-type", "application/json;charset=UTF-8")
 
-        String teacherIdParam = routingContext.request().getParam("id")
-        if (teacherIdParam == null) {
-            response.end("Id param can't be null")
-        } else {
-            try {
-                teachers.remove(teacherIdParam)
-                response.end("Delete successfully teacher's id: " + teacherIdParam)
-            } catch (e) {
-                System.err.println("Error: " + e)
-                response.end("Delete failure. Error: " + e)
+        try {
+            String teacherIdParam = routingContext.request().getParam("id")
+
+            if (teachersByID.get(teacherIdParam).equals(null)) {
+                response.end("This id doesn't existed")
+                return
             }
+            teachersByID.remove(teacherIdParam)
+            response.end("Delete successfully teacher's id: " + teacherIdParam)
+        } catch (e) {
+            System.err.println("Error: " + e)
+            response.end("Delete failure. Error: " + e)
         }
+
     }
 }
